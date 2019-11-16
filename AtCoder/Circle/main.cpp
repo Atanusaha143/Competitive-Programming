8405def7893d986cf768068d7bd9e1fa6c5d12ce
Atanusaha143 <Atanusaha143@github.com>
2019-11-17 01:50:52 +0600

    AtCoder Problem's Solutions

---
+++ AtCoder/Circle/main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+#define C char
+#define D double
+#define F float
+#define LL long long
+#define ULLI unsigned long long int
+#define LLI long long int
+#define LLD long long double
+#define LD long double
+#define S string
+#define PI (2.0*acos(0.0))
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define READ 		freopen("input.txt","r",stdin)
+#define WRITE  		freopen("output.txt","w",stdout)
+using namespace std;
+
+int main()
+
+{
+    IOS;
+
+    LL x;
+    cin>>x;
+    LL z = x*x;
+    cout<<z<<endl;
+
+    return 0;
+}
