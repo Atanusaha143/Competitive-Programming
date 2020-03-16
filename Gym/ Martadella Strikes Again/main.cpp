73ca1b6c4de41b4fbb065e9f4fe6f3d0125c44fa
Atanusaha143 <Atanusaha143@github.com>
2020-03-16 23:30:15 +0600

    OJ related things

---
+++ Gym/ Martadella Strikes Again/main.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define NL '\n'
+#define C char
+#define D double
+#define F float
+#define S string
+#define sz(x) x.size()
+#define INF LONG_LONG_MAX
+#define PI (2.0*acos(0.0))
+#define sfi(x) scanf("%d",&x)
+#define sfi2(x,y) scanf("%d %d",&x,&y)
+#define sfi3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sfl(x) scanf("%lld",&x)
+#define sfl2(x,y) scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x) scanf("%lf",&x);
+#define max3(a,b,c) max(a,max(b,c))
+#define max4(a,b,c,d) max(max3(a,b,c),d)
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define READ freopen("input.txt","r",stdin)
+#define WRITE freopen("output.txt","w",stdout)
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+/*LL gcd(LL a,LL b)
+{
+    if(b==0)
+        return a;
+    else
+        return gcd(b,a%b);
+}
+LL lcm(LL a,LL b)
+{
+    return a/gcd(a,b)*b;
+}*/
+
+LL bigmod(LL base, LL power, LL mod)
+{
+    if(power==0) return 1;
+    else if(power%2==1)
+    {
+        LL value1 = base % mod;
+        LL value2 = (bigmod(base,power-1,mod))%mod;
+        return (value1*value2)%mod;
+    }
+    else
+    {
+        LL value1 = (bigmod(base,power/2,mod))%mod;
+        return (value1*value1)%mod;
+    }
+}
+
+
+int main()
+{
+    LL t;
+    sfl(t);
+    while(t--)
+    {
+        LL a,b;
+        sfl2(a,b);
+        LD one = PI * a * a;
+        LD two = (PI * b * b) + (PI * b * b);
+        if(one>two) cout<<1<<NL;
+        else cout<<2<<NL;
+    }
+}
---
+++ Gym/Ants/main.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define NL '\n'
+#define C char
+#define D double
+#define F float
+#define S string
+#define sz(x) x.size()
+#define INF LONG_LONG_MAX
+#define PI (2.0*acos(0.0))
+#define sfi(x) scanf("%d",&x)
+#define sfi2(x,y) scanf("%d %d",&x,&y)
+#define sfi3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sfl(x) scanf("%lld",&x)
+#define sfl2(x,y) scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x) scanf("%lf",&x);
+#define max3(a,b,c) max(a,max(b,c))
+#define max4(a,b,c,d) max(max3(a,b,c),d)
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+#define unq(vec) vec.erase(unique(vec.begin(),vec.end()),vec.end())
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define READ freopen("input.txt","r",stdin)
+#define WRITE freopen("output.txt","w",stdout)
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+/*LL gcd(LL a,LL b)
+{
+    if(b==0)
+        return a;
+    else
+        return gcd(b,a%b);
+}
+LL lcm(LL a,LL b)
+{
+    return a/gcd(a,b)*b;
+}*/
+
+LL bigmod(LL base, LL power, LL mod)
+{
+    if(power==0) return 1;
+    else if(power%2==1)
+    {
+        LL value1 = base % mod;
+        LL value2 = (bigmod(base,power-1,mod))%mod;
+        return (value1*value2)%mod;
+    }
+    else
+    {
+        LL value1 = (bigmod(base,power/2,mod))%mod;
+        return (value1*value1)%mod;
+    }
+}
+
+bool arr[1000006];
+
+int main()
+{
+    int n;
+    sfi(n);
+    LL ans = 0;
+
+    while(n--)
+    {
+        S s;
+        cin>>s;
+
+        if(s[0]=='-' || sz(s)>=7) continue;
+
+        stringstream sso;
+        sso << s;
+        LL nums;
+        sso >> nums;
+        arr[nums] = true;
+    }
+
+    for(int i=0; i<=1000006; i++)
+    {
+        if(!arr[i])
+        {
+            cout<<i<<NL;
+            break;
+        }
+    }
+}
