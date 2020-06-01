b5d4ea111cd0a2b37597b9f80ddb23c28c33ae5a
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2020-06-01 10:16:53 +0600

    Prime Factorization

---
+++ UVA/Largest Prime Divisor/main.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define NL '\n'
+#define C char
+#define D double
+#define F float
+#define S string
+
+#define sz(x) x.size()
+#define INF LONG_MAX
+#define IINF LLONG_MAX
+#define PI (2.0*acos(0.0))
+
+#define sfi(x) scanf("%d",&x)
+#define sfi2(x,y) scanf("%d %d",&x,&y)
+#define sfi3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sfl(x) scanf("%lld",&x)
+#define sfl2(x,y) scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x) scanf("%lf",&x);
+#define sfd2(x,y) scanf("%lf %lf",&x,&y);
+
+#define max3(a,b,c) max(a,max(b,c))
+#define max4(a,b,c,d) max(max3(a,b,c),d)
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+
+#define fi first
+#define se second
+#define PB push_back
+#define MP make_pair
+#define For(i, j, k) for(int i = j; i <= k; i++)
+#define Rof(i, j, k) for(int i = j; i >= k; i--)
+#define all(x) x.begin(),x.end()
+#define asort(x) sort(all(x));
+#define dsort(x) sort(all(x), greater<int>())
+#define unq(x) x.erase(unique(x.begin(),x.end()),x.end())
+
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define READ freopen("input.txt","r",stdin)
+#define WRITE freopen("output.txt","w",stdout)
+#define IOS ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int,int> PII;
+
+LL gcd(LL a,LL b)
+{
+    if(b==0) return a;
+    else return gcd(b,a%b);
+}
+LL lcm(LL a,LL b)
+{
+    return a/gcd(a,b)*b;
+}
+
+LL bigmod(LL base, LL power, LL mod)
+{
+    if(power==0) return 1;
+    else if(power%2==1)
+    {
+        LL value1 = base % mod; LL value2 = (bigmod(base,power-1,mod))%mod;
+        return (value1*value2)%mod;
+    }
+    else
+    {
+        LL value1 = (bigmod(base,power/2,mod))%mod;
+        return (value1*value1)%mod;
+    }
+}
+
+void primeFact(LL n)
+{
+        set <LL> st;
+        LL limit = sqrt(n);
+        LL maxi = 0;
+        while(n%2==0)
+        {
+            maxi = 2;
+            n /= 2;
+            st.insert(2);
+        }
+        for(int i=3; i<=limit; i+=2)
+        {
+            while(n%i==0)
+            {
+                maxi = i;
+                n /= i;
+                st.insert(i);
+            }
+        }
+        if(n>2) maxi = n, st.insert(maxi);
+        if(sz(st)<2) printf("-1\n");
+        else printf("%lld\n",maxi);
+}
+int main()
+{
+
+    LL n;
+    while(sfl(n) and n)
+    {
+        if(n<0) n *= -1;
+        primeFact(n);
+    }
+}
