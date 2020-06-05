958c77db68c4654aa6ab967eefb1f8810a555006
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2020-06-05 20:21:40 +0600

    Cumulative Sum of Divisors

---
+++ LightOJ/A New Function/main.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define NL '\n'
+#define C char
+#define D double
+#define F float
+#define S string
+
+#define sz(x) x.size()
+#define INF LONG_MAX
+#define IINF LLONG_MAX
+#define PI (2.0*acos(0.0))
+
+#define sfi(x) scanf("%d",&x)
+#define sfi2(x,y) scanf("%d %d",&x,&y)
+#define sfi3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sfl(x) scanf("%lld",&x)
+#define sfl2(x,y) scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x) scanf("%lf",&x);
+#define sfd2(x,y) scanf("%lf %lf",&x,&y);
+
+#define max3(a,b,c) max(a,max(b,c))
+#define max4(a,b,c,d) max(max3(a,b,c),d)
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+
+#define fi first
+#define se second
+#define PB push_back
+#define MP make_pair
+#define For(i, j, k) for(int i = j; i <= k; i++)
+#define Rof(i, j, k) for(int i = j; i >= k; i--)
+#define all(x) x.begin(),x.end()
+#define asort(x) sort(all(x));
+#define dsort(x) sort(all(x), greater<int>())
+#define unq(x) x.erase(unique(x.begin(),x.end()),x.end())
+
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define READ freopen("input.txt","r",stdin)
+#define WRITE freopen("output.txt","w",stdout)
+#define IOS ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int,int> PII;
+
+LL gcd(LL a,LL b)
+{
+    if(b==0) return a;
+    else return gcd(b,a%b);
+}
+LL lcm(LL a,LL b)
+{
+    return a/gcd(a,b)*b;
+}
+
+LL bigmod(LL base, LL power, LL mod)
+{
+    if(power==0) return 1;
+    else if(power%2==1)
+    {
+        LL value1 = base % mod; LL value2 = (bigmod(base,power-1,mod))%mod;
+        return (value1*value2)%mod;
+    }
+    else
+    {
+        LL value1 = (bigmod(base,power/2,mod))%mod;
+        return (value1*value1)%mod;
+    }
+}
+
+int main()
+{
+    int t;
+    sfi(t);
+
+    For(i,1,t)
+    {
+        LL n;
+        sfl(n);
+
+        if(n==0)
+        {
+            printf("Case %d: 0\n",i);
+            continue;
+        }
+        LL limit = sqrt(n);
+        LL sum = 0;
+        For(j,1,limit)
+        {
+            LL half = n/j;
+            sum += (half*(half+1))/2;
+            sum -= ((j-1)*(j-1+1))/2;
+            sum += (half-j)*j;
+        }
+        LL deduct = ((n*(n+1))/2)+(n-1);
+        LL remain = sum - deduct;
+
+        printf("Case %d: %lld\n",i,remain);
+    }
+}
