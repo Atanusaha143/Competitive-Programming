2e45176bac73b715c1799d95c01bc9dc669c209b
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2020-06-13 02:08:26 +0600

    Long Contest

---
+++ Codechef/June Challenge 2020 Division 2/The Tom and Jerry Game!.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define NL '\n'
+#define sz(x) x.size()
+#define INF LONG_MAX
+#define IINF LLONG_MAX
+#define PI (2.0*acos(0.0))
+
+#define sfi(x) scanf("%d",&x)
+#define sfi2(x,y) scanf("%d %d",&x,&y)
+#define sfi3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sfl(x) scanf("%lld",&x)
+#define sfl2(x,y) scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x) scanf("%lf",&x);
+#define sfd2(x,y) scanf("%lf %lf",&x,&y);
+
+#define max3(a,b,c) max(a,max(b,c))
+#define max4(a,b,c,d) max(max3(a,b,c),d)
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+
+#define fi first
+#define se second
+#define PB push_back
+#define MP make_pair
+#define For(i, j, k) for(int i = j; i <= k; i++)
+#define Rof(i, j, k) for(int i = j; i >= k; i--)
+#define all(x) x.begin(),x.end()
+#define asort(x) sort(all(x));
+#define dsort(x) sort(all(x), greater<int>())
+#define unq(x) x.erase(unique(x.begin(),x.end()),x.end())
+
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define READ freopen("input.txt","r",stdin)
+#define WRITE freopen("output.txt","w",stdout)
+#define IOS ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+LL gcd(LL a,LL b) { return b==0 ? a : gcd(b,a%b); }
+LL lcm(LL a,LL b) { return (a/gcd(a,b))*b; }
+
+bool check(LL n)
+{
+    int limit = sqrt(n);
+
+    for(int i=2; i<=limit; i++)
+    {
+        if(i*i==n and i&1) return true;
+        else if(n%i==0)
+        {
+            if(i&1 or (n/i)&1) return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int t;
+    sfi(t);
+
+    while(t--)
+    {
+        LL n;
+        sfl(n);
+
+        if(n&1) printf("%lld\n",n/2);
+        else
+        {
+           bool kiObostha = check(n);
+           if(!kiObostha) printf("0\n");
+           else
+           {
+               LL step = 0, copyN = n;
+               while(n%2==0)
+               {
+                   n /= 2;
+                   step++;
+               }
+               LL start1 = pow(2,step)*2;
+               LL count = copyN/start1;
+               printf("%lld\n",count);
+           }
+        }
+    }
+}
