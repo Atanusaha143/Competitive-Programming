f91b811071c619c624f6c7667eb2d5b0e90ac6e8
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2020-09-27 01:58:44 +0600

    Binary Search

---
+++ SPOJ/AGGRCOW - Aggressive cows.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define NL '\n'
+#define sz(x) x.size()
+#define INF LONG_MAX
+#define IINF LLONG_MAX
+#define PI (2.0*acos(0.0))
+
+#define sfi(x) scanf("%d",&x)
+#define sfi2(x,y) scanf("%d %d",&x,&y)
+#define sfi3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sfl(x) scanf("%lld",&x)
+#define sfl2(x,y) scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x) scanf("%lf",&x);
+#define sfd2(x,y) scanf("%lf %lf",&x,&y);
+
+#define max3(a,b,c) max(a,max(b,c))
+#define max4(a,b,c,d) max(max3(a,b,c),d)
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+
+#define fi first
+#define se second
+#define PB push_back
+#define MP make_pair
+#define For(i, j, k) for(int i = j; i <= k; i++)
+#define Rof(i, j, k) for(int i = j; i >= k; i--)
+#define all(x) x.begin(),x.end()
+#define asort(x) sort(all(x));
+#define dsort(x) sort(all(x), greater<int>())
+#define unq(x) x.erase(unique(x.begin(),x.end()),x.end())
+
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define READ freopen("input.txt","r",stdin)
+#define WRITE freopen("output.txt","w",stdout)
+#define IOS ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+LL gcd(LL a,LL b) { return b==0 ? a : gcd(b,a%b); }
+LL lcm(LL a,LL b) { return (a/gcd(a,b))*b; }
+
+int main()
+{
+    int t;
+    sfi(t);
+
+    while(t--)
+    {
+        int n, c;
+        sfi2(n,c);
+        vector <int> vec(n);
+        For(i,0,n-1) sfi(vec[i]);
+        asort(vec);
+
+        int l = 0, h = vec[n-1] - vec[0], ans = 0;
+        while(l<=h)
+        {
+            int mid = l + (h - l) / 2;
+
+            int pre = vec[0], count = 1;
+            For(i,1,n-1)
+                if(vec[i] - pre >= mid)
+                    pre = vec[i], count++;
+
+            if(count>=c) l = mid + 1, ans = max(ans,mid);
+            else h = mid - 1;
+        }
+        printf("%d\n", ans);
+    }
+}
+
+/*
+
+1
+4 3
+4 3 5 10
+
+1
+5 5
+1 2 3 4 5
+
+*/
+
