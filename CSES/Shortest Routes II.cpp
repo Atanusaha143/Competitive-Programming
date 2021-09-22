8afd2a3711b73887ece9c85c6976df31b6332059
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2021-09-22 16:37:48 +0600

    Minimum Distance, Floyd-Warshall

---
+++ CSES/Shortest Routes II.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NL '\n'
+#define sz(x) x.size()
+#define PINF INT_MAX
+#define NINF INT_MIN
+#define PI (2.0*acos(0.0))
+
+#define sfi(x) scanf("%d",&x)
+#define sfi2(x,y) scanf("%d %d",&x,&y)
+#define sfi3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sfl(x) scanf("%lld",&x)
+#define sfl2(x,y) scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x) scanf("%lf",&x);
+#define sfd2(x,y) scanf("%lf %lf",&x,&y);
+
+#define max3(a,b,c) max(a,max(b,c))
+#define max4(a,b,c,d) max(max3(a,b,c),d)
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+
+#define fi first
+#define se second
+#define PB push_back
+#define PF push_front
+#define MP make_pair
+#define For(i, j, k) for(int i = j; i <= k; i++)
+#define Rof(i, j, k) for(int i = j; i >= k; i--)
+#define all(x) x.begin(),x.end()
+#define asort(x) sort(all(x));
+#define dsort(x) sort(all(x), greater<int>())
+#define unq(x) x.erase(unique(x.begin(),x.end()),x.end())
+#define now cout<<"Here"<<NL;
+
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define READ freopen("input.txt","r",stdin)
+#define WRITE freopen("output.txt","w",stdout)
+#define IOS ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+LL gcd(LL a,LL b) { return b==0 ? a : gcd(b,a%b); }
+LL lcm(LL a,LL b) { return (a/gcd(a,b))*b; }
+bool compare(string &str1, string &str2) { return str1.size() > str2.size(); }
+
+// Grid direction array [4]
+int X[4]={0,0,-1,1};
+int Y[4]={1,-1,0,0};
+// Grid direction array [8]
+int X8[8]={0,0,1,-1,-1,1,1,-1};
+int Y8[8]={1,-1,0,0,-1,-1,1,1};
+// Knight Direction Array
+int KX[8] = {1,1,2,2,-1,-1,-2,-2};
+int KY[8] = {2,-2,1,-1,2,-2,1,-1};
+
+const int N = 505;
+LL dist[N][N];
+int n,m,q;
+
+void FW()
+{
+    for(int k=1; k<=n; k++)
+        for(int i=1; i<=n; i++)
+            for(int j=1; j<=n; j++)
+                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
+}
+
+int main()
+{
+    for(int i=1; i<=N; i++)
+        for(int j=1; j<=N; j++)
+            if(i!=j)
+                dist[i][j] = 1e17;
+
+    sfi3(n,m,q);
+    For(i,1,m)
+    {
+        LL a,b,c;
+        sfl3(a,b,c);
+        dist[a][b] = min(dist[a][b],c);
+        dist[b][a] = min(dist[b][a],c);
+    }
+
+    FW();
+
+    while(q--)
+    {
+        int from, to;
+        sfi2(from,to);
+        if(dist[from][to] == 1e17) printf("-1\n");
+        else printf("%lld\n", dist[from][to]);
+    }
+
+    //main();
+}
+
+/*
+
+3
+3
+1 2 100
+2 3 200
+1 3 50
+
+*/
+
