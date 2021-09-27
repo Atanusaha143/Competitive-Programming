8354e27455dd05045b9ac00c8d44ff7c7c83fdfc
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2021-09-27 09:34:57 +0600

    Graph Traversal, 0-1 BFS

---
+++ SPOJ/KATHTHI - KATHTHI.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NL '\n'
+#define sz(x) x.size()
+#define PINF INT_MAX
+#define NINF INT_MIN
+#define PI (2.0*acos(0.0))
+
+#define sfi(x) scanf("%d",&x)
+#define sfi2(x,y) scanf("%d %d",&x,&y)
+#define sfi3(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sfl(x) scanf("%lld",&x)
+#define sfl2(x,y) scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x) scanf("%lf",&x);
+#define sfd2(x,y) scanf("%lf %lf",&x,&y);
+
+#define max3(a,b,c) max(a,max(b,c))
+#define max4(a,b,c,d) max(max3(a,b,c),d)
+#define min3(a,b,c) min(a,min(b,c))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+
+#define fi first
+#define se second
+#define PB push_back
+#define PF push_front
+#define MP make_pair
+#define For(i, j, k) for(int i = j; i <= k; i++)
+#define Rof(i, j, k) for(int i = j; i >= k; i--)
+#define all(x) x.begin(),x.end()
+#define asort(x) sort(all(x));
+#define dsort(x) sort(all(x), greater<int>())
+#define unq(x) x.erase(unique(x.begin(),x.end()),x.end())
+#define now cout<<"Here"<<NL;
+
+#define mem(ara,val) memset(ara,val,sizeof(ara))
+#define READ freopen("input.txt","r",stdin)
+#define WRITE freopen("output.txt","w",stdout)
+#define IOS ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+LL gcd(LL a,LL b) { return b==0 ? a : gcd(b,a%b); }
+LL lcm(LL a,LL b) { return (a/gcd(a,b))*b; }
+bool compare(string &str1, string &str2) { return str1.size() > str2.size(); }
+
+// Grid direction array [4]
+int X[4]={0,0,-1,1};
+int Y[4]={1,-1,0,0};
+// Grid direction array [8]
+int X8[8]={0,0,1,-1,-1,1,1,-1};
+int Y8[8]={1,-1,0,0,-1,-1,1,1};
+// Knight Direction Array
+int KX[8] = {1,1,2,2,-1,-1,-2,-2};
+int KY[8] = {2,-2,1,-1,2,-2,1,-1};
+
+const int N = 1005;
+char mat[N][N];
+int n,m;
+bool visited[N][N];
+int dist[N][N];
+
+bool check(int x, int y)
+{
+    return (x>=0 and x<n and y>=0 and y<m and !visited[x][y]);
+}
+
+void bfs(int startX, int startY)
+{
+    visited[startX][startY] = true;
+    dist[startX][startY] = 0;
+    deque <PII> q;
+    q.push_front({startX,startY});
+
+    while(!q.empty())
+    {
+        PII u = q.front();
+        q.pop_front();
+
+        int x = u.first;
+        int y = u.second;
+
+        For(i,0,3)
+        {
+            int newX = X[i] + x;
+            int newY = Y[i] + y;
+
+            if(check(newX,newY))
+            {
+                int cost = 0;
+                if(mat[newX][newY] != mat[x][y])
+                    cost = 1;
+
+                if(dist[x][y] + cost < dist[newX][newY])
+                {
+                    dist[newX][newY] = dist[x][y] + cost;
+                    if(cost == 0) q.push_front({newX,newY});
+                        else q.push_back({newX,newY});
+                }
+            }
+        }
+    }
+}
+
+void clean(int n, int m)
+{
+    For(i,0,n-1)
+        For(j,0,m-1)
+            visited[i][j] = false;
+    For(i,0,n-1)
+        For(j,0,m-1)
+            dist[i][j] = 1e9;
+}
+
+int main()
+{
+    int t;
+    sfi(t);
+    while(t--)
+    {
+        sfi2(n,m);
+        For(i,0,n-1)
+        For(j,0,m-1)
+        cin>>mat[i][j];
+
+        clean(n,m);
+
+        bfs(0,0);
+
+        printf("%d\n", dist[n-1][m-1]);
+    }
+
+    //main();
+}
+
+/*
+
+4
+2 2
+aa
+aa
+2 3
+abc
+def
+6 6
+akaccc
+aaacfc
+amdfcc
+aokhdd
+zyxwdp
+zyxwdd
+5 5
+abbbc
+abacc
+aaacc
+aefci
+cdgdd
+
+*/
+
