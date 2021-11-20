b198d3b9ff5c0739f63663d2bf5757a6d951140a
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2021-11-21 00:03:40 +0600

    BFS on Grid

---
+++ CSES/Counting Rooms.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NL              '\n'
+#define sz(x)           x.size()
+#define PINF            INT_MAX
+#define NINF            INT_MIN
+#define PI              (2.0*acos(0.0))
+
+#define sfi(x)          scanf("%d",&x)
+#define sfi2(x,y)       scanf("%d %d",&x,&y)
+#define sfi3(x,y,z)     scanf("%d %d %d",&x,&y,&z)
+#define sfl(x)          scanf("%lld",&x)
+#define sfl2(x,y)       scanf("%lld %lld",&x,&y)
+#define sfl3(x,y,z)     scanf("%lld %lld %lld",&x,&y,&z)
+#define sfd(x)          scanf("%lf",&x);
+#define sfd2(x,y)       scanf("%lf %lf",&x,&y);
+
+#define max3(a,b,c)     max(a,max(b,c))
+#define max4(a,b,c,d)   max(max3(a,b,c),d)
+#define min3(a,b,c)     min(a,min(b,c))
+#define min4(a,b,c,d)   min(a,min3(b,c,d))
+
+#define PB              push_back
+#define MP              make_pair
+#define For(i, j, k)    for(int i = j; i <= k; i++)
+#define Rof(i, j, k)    for(int i = j; i >= k; i--)
+#define all(x)          x.begin(),x.end()
+#define asort(x)        sort(all(x));
+#define dsort(x)        sort(all(x), greater<int>())
+#define now             cout<<"Here"<<NL;
+
+#define mem(ara,val)    memset(ara,val,sizeof(ara))
+#define READ            freopen("input.txt","r",stdin)
+#define WRITE           freopen("output.txt","w",stdout)
+#define IOS             ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+LL gcd(LL a,LL b) { return b==0 ? a : gcd(b,a%b); }
+LL lcm(LL a,LL b) { return (a/gcd(a,b))*b; }
+
+LL power_mod(LL base, LL power, LL mod)
+{
+    if(power==0) return 1LL;
+    else if(power%2LL == 1)
+    {
+        LL p1 = base % mod;
+        LL p2 = (power_mod(base,power-1,mod))%mod;
+        return (p1*p2)%mod;
+    }
+    else if(power%2LL == 0)
+    {
+        LL p1 = (power_mod(base,power/2,mod))%mod;
+        return (p1*p1)%mod;
+    }
+}
+
+// Grid direction array [4]
+int X[4]= {0,0,-1,1};
+int Y[4]= {1,-1,0,0};
+// Grid direction array [8]
+int X8[8]= {0,0,1,-1,-1,1,1,-1};
+int Y8[8]= {1,-1,0,0,-1,-1,1,1};
+// Knight Direction Array
+int KX[8] = {1,1,2,2,-1,-1,-2,-2};
+int KY[8] = {2,-2,1,-1,2,-2,1,-1};
+
+const int N = 1e3 + 10;
+int n,m;
+char mat[N][N];
+bool visited[N][N];
+//const LL MOD = 1000000007;
+
+bool check(int x, int y)
+{
+    return (x >= 0 and x < n and y >=0 and y < m and !visited[x][y] and mat[x][y] == '.');
+}
+
+void bfs(int startX, int startY)
+{
+    queue<PII> q;
+    q.push({startX,startY});
+    visited[startX][startY] = true;
+
+    while(!q.empty())
+    {
+        PII curr =q.front(); q.pop();
+
+        For(i,0,3)
+        {
+            int newX = curr.first + X[i];
+            int newY = curr.second + Y[i];
+
+            if(check(newX,newY))
+            {
+                visited[newX][newY] = true;
+                q.push({newX,newY});
+            }
+        }
+    }
+}
+
+int main()
+{
+
+//    int t; sfi(t); For(tt,1,t){}
+
+    sfi2(n,m);
+    For(i,0,n-1)
+    For(j,0,m-1)
+    cin>>mat[i][j];
+
+    int cnt = 0;
+    For(i,0,n-1)
+    {
+        For(j,0,m-1)
+        {
+            if(mat[i][j] == '.' and !visited[i][j])
+            {
+                bfs(i,j);
+                cnt++;
+            }
+        }
+    }
+
+    printf("%d", cnt);
+
+    //main();
+}
+
+/*
+
+
+*/
+
