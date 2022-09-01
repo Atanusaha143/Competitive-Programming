e911bc0916a52bbb292eafe5a6c662eb72404775
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2022-09-01 20:46:43 +0600

    Suffix Array

---
+++ LightOJ/Names for Babies.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+#define endl              '\n'
+#define PI              (2.0*acos(0.0))
+#define PB              push_back
+#define MP              make_pair
+#define all(x)          x.begin(),x.end()
+#define asort(x)        sort(all(x));
+#define dsort(x)        sort(all(x), greater<int>())
+#define unq(x)          x.erase(unique(x.begin(),x.end()),x.end())
+#define popcountl       __builtin_popcountll
+#define popcount        __builtin_popcount
+#define now             cout<<"Here"<<endl;
+#define mem(ara,val)    memset(ara,val,sizeof(ara))
+#define READ            freopen("input.txt","r",stdin)
+#define WRITE           freopen("output.txt","w",stdout)
+#define IOS             ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// *find_by_order = value of an index, order_of_key = index of a value where it should be
+//typedef tree<PII, null_type, less<PII>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+inline bool checkBit(int N, int pos) { return (bool)(N & (1 << pos)); }
+inline int setBit(int N, int pos) { return N = N | (1 << pos); }
+inline int toggleBit(int N, int pos) { return N = (N ^ (1 << pos)); }
+
+LL gcd(LL a, LL b) { return b==0 ? a : gcd(b, a % b); }
+LL lcm(LL a, LL b) { return (a / gcd(a, b)) * b; }
+
+//LL power_mod(LL base, LL power, LL mod){
+//    if(power==0) return 1LL;
+//    else if(power%2LL == 1){
+//        LL p1 = base % mod; LL p2 = (power_mod(base,power-1,mod))%mod;
+//        p1 = (LL)(p1*p2); return p1%mod;
+//    }
+//    else if(power%2LL == 0){
+//        LL p1 = (power_mod(base,power/2LL,mod))%mod; p1 = (LL)(p1*p1);
+//        return p1%mod;
+//    }
+//}
+
+//LL original_power(LL base, LL power){
+//    if(power==0) return 1LL;
+//    else if(power%2LL == 1){
+//        LL p1 = base; LL p2 = (original_power(base,power-1));
+//        p1 = (LL)(p1*p2); return p1;
+//    }
+//    else if(power%2LL == 0){
+//        LL p1 = (original_power(base,power/2LL)); p1 = (LL)(p1*p1);
+//        return p1;
+//    }
+//}
+
+//// Grid direction array [4]
+//int X[4]= {0,0,-1,1};
+//int Y[4]= {1,-1,0,0};
+// Grid direction array [8]
+//int X8[8]= {0,0,1,-1,-1,1,1,-1};
+//int Y8[8]= {1,-1,0,0,-1,-1,1,1};
+//// Knight Direction Array
+//int KX[8] = {1,1,2,2,-1,-1,-2,-2};
+//int KY[8] = {2,-2,1,-1,2,-2,1,-1};
+
+///------------------------------------------------------------------------------------------------------
+
+const int N = 2e5 + 10;
+const int MOD = 1000000007;
+
+#define MAX 2500000
+
+int wa[MAX],wb[MAX],wv[MAX],Ws[MAX];
+int cmp(int *r,int a,int b,int l) {return r[a]==r[b] && r[a+l]==r[b+l];}
+
+//(1-indexed) sa[i] = starting position (0...n-1) of ith lexicographically smallest suffix in s
+//(0-indexed) Rank[i] = lexicographical rank of s[i....n-1] ((i+1)th suffix by position)
+//LCP[i] = longest common prefix of sa[i] & sa[i-1]
+int sa[MAX],Rank[MAX],LCP[MAX];
+
+//Suffix Array (O(nlogn))
+//m = maximum possible ASCII value of a string character (alphabet size)
+//also, m = maximum number of distinct character in string (when compressed)
+void buildSA(string s,int* sa,int n,int m){
+    int i,j,p,*x=wa,*y=wb,*t;
+    for(i=0; i<m; i++) Ws[i]=0;
+    for(i=0; i<n; i++) Ws[x[i]=s[i]]++;
+    for(i=1; i<m; i++) Ws[i]+=Ws[i-1];
+    for(i=n-1; i>=0; i--) sa[--Ws[x[i]]]=i;
+    for(j=1,p=1; p<n; j<<=1,m=p){
+        for(p=0,i=n-j; i<n; i++) y[p++]=i;
+        for(i=0; i<n; i++) if(sa[i]>=j) y[p++]=sa[i]-j;
+        for(i=0; i<n; i++) wv[i]=x[y[i]];
+        for(i=0; i<m; i++) Ws[i]=0;
+        for(i=0; i<n; i++) Ws[wv[i]]++;
+        for(i=1; i<m; i++) Ws[i]+=Ws[i-1];
+        for(i=n-1; i>=0; i--) sa[--Ws[wv[i]]]=y[i];
+        for(t=x,x=y,y=t,p=1,x[sa[0]]=0,i=1; i<n; i++)
+            x[sa[i]]=cmp(y,sa[i-1],sa[i],j) ? p-1 : p++;
+    }
+}
+
+//Kasai's LCP algorithm (O(n))
+void buildLCP(string s,int *sa,int n){
+    int i,j,k=0;
+    for(i=1; i<=n; i++) Rank[sa[i]]=i;
+    for(i=0; i<n; LCP[Rank[i++]]=k)
+        for(k?k--:0, j=sa[Rank[i]-1]; s[i+k]==s[j+k]; k++);
+    return;
+}
+
+
+void solve(int casenum)
+{
+    string s;
+    cin>>s;
+    LL l, r;
+    cin>>l>>r;
+    LL n=s.size();
+    buildSA(s,sa,n+1,130); //Important
+    buildLCP(s,sa,n);
+
+    LL ans = 0;
+    for(int i=1; i<=n; i++)
+    {
+        LL amarL = LCP[i] + 1;
+        LL amarR = n - sa[i];
+        amarL = max(l,amarL);
+        amarR = min(r,amarR);
+        ans += max(0LL,(amarR-amarL)+1);
+    }
+    cout<<"Case "<<casenum<<": "<<ans<<endl;
+}
+
+int main()
+{
+    IOS;
+
+//    READ;
+//    WRITE;
+
+    int t = 1;
+    cin>>t;
+    for(int tt=1; tt<=t; tt++){
+        solve(tt);
+    }
+
+    return 0;
+}
+
+/*
+
+Link : https://lightoj.com/problem/names-for-babies
+
+*/
+
