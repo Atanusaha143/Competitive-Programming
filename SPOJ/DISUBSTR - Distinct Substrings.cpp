a642b8de9497e7f858bec13f43255268af8c5419
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2022-09-21 03:28:52 +0600

    Basic Suffix Array Problem

---
+++ SPOJ/DISUBSTR - Distinct Substrings.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl            '\n'
+#define PI              (2.0*acos(0.0))
+#define PB              push_back
+#define MP              make_pair
+#define all(x)          x.begin(),x.end()
+#define asort(x)        sort(all(x));
+#define dsort(x)        sort(all(x), greater<int>())
+#define unq(x)          x.erase(unique(x.begin(),x.end()),x.end())
+#define popcountl       __builtin_popcountll
+#define popcount        __builtin_popcount
+#define now             cerr<<"Here"<<endl;
+#define mem(ara,val)    memset(ara,val,sizeof(ara))
+#define IOS             ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+inline bool checkBit(int N, int pos) { return (bool)(N & (1 << pos)); }
+inline int setBit(int N, int pos) { return N = N | (1 << pos); }
+inline int toggleBit(int N, int pos) { return N = (N ^ (1 << pos)); }
+
+LL gcd(LL a, LL b) { return b==0 ? a : gcd(b, a % b); }
+LL lcm(LL a, LL b) { return (a / gcd(a, b)) * b; }
+
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+///------------------------------------------------------------------------------------------------------------------------
+
+const int N = 1e6 + 10;
+const int MOD = 1000000007;
+
+#define MAX 1000005
+int wa[MAX],wb[MAX],wv[MAX],Ws[MAX];
+int cmp(int *r,int a,int b,int l) {return r[a]==r[b] && r[a+l]==r[b+l];}
+
+//(1-indexed) sa[i] = starting position (0...n-1) of ith lexicographically smallest suffix in s
+//(0-indexed) Rank[i] = lexicographical rank of s[i....n-1] ((i+1)th suffix by position)
+//LCP[i] = longest common prefix of sa[i] & sa[i-1]
+int sa[MAX],Rank[MAX],LCP[MAX];
+
+//Suffix Array (O(nlogn))
+//m = maximum possible ASCII value of a string character (alphabet size)
+//also, m = maximum number of distinct character in string (when compressed)
+void buildSA(string s,int* sa,int n,int m){
+    int i,j,p,*x=wa,*y=wb,*t;
+    for(i=0; i<m; i++) Ws[i]=0;
+    for(i=0; i<n; i++) Ws[x[i]=s[i]]++;
+    for(i=1; i<m; i++) Ws[i]+=Ws[i-1];
+    for(i=n-1; i>=0; i--) sa[--Ws[x[i]]]=i;
+    for(j=1,p=1; p<n; j<<=1,m=p){
+        for(p=0,i=n-j; i<n; i++) y[p++]=i;
+        for(i=0; i<n; i++) if(sa[i]>=j) y[p++]=sa[i]-j;
+        for(i=0; i<n; i++) wv[i]=x[y[i]];
+        for(i=0; i<m; i++) Ws[i]=0;
+        for(i=0; i<n; i++) Ws[wv[i]]++;
+        for(i=1; i<m; i++) Ws[i]+=Ws[i-1];
+        for(i=n-1; i>=0; i--) sa[--Ws[wv[i]]]=y[i];
+        for(t=x,x=y,y=t,p=1,x[sa[0]]=0,i=1; i<n; i++)
+            x[sa[i]]=cmp(y,sa[i-1],sa[i],j) ? p-1 : p++;
+    }
+}
+
+//Kasai's LCP algorithm (O(n))
+void buildLCP(string s,int *sa,int n){
+    int i,j,k=0;
+    for(i=1; i<=n; i++) Rank[sa[i]]=i;
+    for(i=0; i<n; LCP[Rank[i++]]=k)
+        for(k?k--:0, j=sa[Rank[i]-1]; s[i+k]==s[j+k]; k++);
+    return;
+}
+
+void solve(int casenum)
+{
+
+    string s;
+    cin>>s;
+
+    int n=s.size();
+    buildSA(s,sa,n+1,130); //Important
+    buildLCP(s,sa,n);
+    // for(int i=1;i<=n;i++) cerr<<sa[i]<<" "<<s.substr(sa[i])<<" "<<LCP[i]<<endl;
+    
+    LL ans = 0;
+    for(int i=1; i<=n; i++)
+    {
+        // if(LCP[i] == 0) ans += LCP[i];
+        // else
+        // {
+            LL amarLCP = LCP[i];
+            LL amarLen = n - sa[i];
+            LL subCnt = amarLen - amarLCP;
+            ans += subCnt;
+        // }
+    }
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    IOS;
+
+    #ifdef sinbadCP
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t = 1;
+    cin>>t;
+    for(int tt=1; tt<=t; tt++){
+        solve(tt);
+    }
+
+    return 0;
+}
+
+/*
+
+Link : https://www.spoj.com/problems/DISUBSTR/
+
+*/
