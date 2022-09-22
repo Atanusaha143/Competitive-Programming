f00c7f83c7e471d86a3c8b4e4c21acac85d3fcce
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2022-09-22 23:47:54 +0600

    Observation and Trick

---
+++ LightOJ/Again Array Queries.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl            '\n'
+#define PI              (2.0*acos(0.0))
+#define PB              push_back
+#define MP              make_pair
+#define all(x)          x.begin(),x.end()
+#define asort(x)        sort(all(x));
+#define dsort(x)        sort(all(x), greater<int>())
+#define unq(x)          x.erase(unique(x.begin(),x.end()),x.end())
+#define popcountl       __builtin_popcountll
+#define popcount        __builtin_popcount
+#define now             cerr<<"Here"<<endl;
+#define mem(ara,val)    memset(ara,val,sizeof(ara))
+#define IOS             ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+inline bool checkBit(int N, int pos) { return (bool)(N & (1 << pos)); }
+inline int setBit(int N, int pos) { return N = N | (1 << pos); }
+inline int toggleBit(int N, int pos) { return N = (N ^ (1 << pos)); }
+
+LL gcd(LL a, LL b) { return b==0 ? a : gcd(b, a % b); }
+LL lcm(LL a, LL b) { return (a / gcd(a, b)) * b; }
+
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+///------------------------------------------------------------------------------------------------------------------------
+
+const int N = 1e6 + 10;
+const int MOD = 1000000007;
+
+int n, q;
+int arr[N];
+
+void solve(int casenum)
+{
+    cin>>n>>q;
+    for(int i=1; i<=n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"Case "<<casenum<<":"<<endl; 
+    while(q--)
+    {
+        int l, r;
+        cin>>l>>r;
+        l++, r++;
+        int diff = r - l;
+        if(diff > 1000)
+        {
+            cout<<"0"<<endl;
+        }
+        else
+        {
+            int len = (r - l) + 1, id = 0;
+            int brr[len+1] =  {0};
+            for(int i=l; i<=r; i++)
+            {
+                brr[++id] = arr[i];
+            }
+            sort(brr+1,brr+id+1);
+            int ans = 2000;
+            for(int i=1; i<id; i++)
+            {
+                int dif = brr[i+1] - brr[i];
+                ans = min( ans,dif );
+            }
+            cout<<ans<<endl;
+        }
+    }
+}
+
+int main()
+{
+    IOS;
+
+    #ifdef sinbadCP
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t = 1;
+    cin>>t;
+    for(int tt=1; tt<=t; tt++){
+        solve(tt);
+    }
+
+    return 0;
+}
+
+/*
+
+Link : https://lightoj.com/problem/again-array-queries
+
+Sol : Observation problem. As the numbers are between 1 to 1000, then
+if any query length greater than 1000 will must have a repeated value.
+So, for this case the answer will be 0.
+If the length of query (r - l + 1) is less than 1000, then we can just sort
+the elements between r to l becasuse consecutive two numbers will have 
+minimum value.
+
+*/
+
