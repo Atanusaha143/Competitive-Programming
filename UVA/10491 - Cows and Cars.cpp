c3f9c6931ca4b123e09b5bd93b40eff80229a64d
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2022-09-29 02:09:42 +0600

    Probability

---
+++ UVA/10491 - Cows and Cars.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl            '\n'
+#define PI              (2.0*acos(0.0))
+#define PB              push_back
+#define MP              make_pair
+#define all(x)          x.begin(),x.end()
+#define asort(x)        sort(all(x));
+#define dsort(x)        sort(all(x), greater<int>())
+#define unq(x)          x.erase(unique(x.begin(),x.end()),x.end())
+#define popcountl       __builtin_popcountll
+#define popcount        __builtin_popcount
+#define now             cerr<<"Here"<<endl;
+#define mem(ara,val)    memset(ara,val,sizeof(ara))
+#define IOS             ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+inline bool checkBit(int N, int pos) { return (bool)(N & (1 << pos)); }
+inline int setBit(int N, int pos) { return N = N | (1 << pos); }
+inline int toggleBit(int N, int pos) { return N = (N ^ (1 << pos)); }
+
+LL gcd(LL a, LL b) { return b==0 ? a : gcd(b, a % b); }
+LL lcm(LL a, LL b) { return (a / gcd(a, b)) * b; }
+
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+///------------------------------------------------------------------------------------------------------------------------
+
+const int N = 1e6 + 10;
+const int MOD = 1000000007;
+
+void solve(int casenum)
+{
+
+    double ncows, ncars, nshows;
+    while(cin>>ncows>>ncars>>nshows)
+    {
+        /// 1st choice - pick a cow and then switch to a car
+        double p1cow = ncows / (ncows+ncars);
+        double p1car = ncars / (ncows+ncars-nshows-1);
+        double p1 = p1cow * p1car;
+
+        // 2nd choice - pick a car and then switch to a car
+        double p2car1 = ncars / (ncows+ncars);
+        double p2car2 = (ncars-1) / (ncows+ncars-nshows-1);
+        double p2 = p2car1 * p2car2;
+
+        double ans = p1 + p2;
+        cout<<fixed<<setprecision(5)<<ans<<endl;
+    }
+    
+}
+
+int main()
+{
+    IOS;
+
+    #ifdef sinbadCP
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t = 1;
+    // cin>>t;
+    for(int tt=1; tt<=t; tt++){
+        solve(tt);
+    }
+
+    return 0;
+}
+
+/*
+
+Link : https://onlinejudge.org/external/104/10491.pdf
+
+*/
