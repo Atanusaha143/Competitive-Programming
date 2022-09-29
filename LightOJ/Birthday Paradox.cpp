3d9d4fda02c603ce17bf3737f060c71384513e0e
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2022-09-29 13:56:28 +0600

    Probability

---
+++ LightOJ/Birthday Paradox.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl            '\n'
+#define PI              (2.0*acos(0.0))
+#define PB              push_back
+#define MP              make_pair
+#define all(x)          x.begin(),x.end()
+#define asort(x)        sort(all(x));
+#define dsort(x)        sort(all(x), greater<int>())
+#define unq(x)          x.erase(unique(x.begin(),x.end()),x.end())
+#define popcountl       __builtin_popcountll
+#define popcount        __builtin_popcount
+#define now             cerr<<"Here"<<endl;
+#define mem(ara,val)    memset(ara,val,sizeof(ara))
+#define IOS             ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+inline bool checkBit(int N, int pos) { return (bool)(N & (1 << pos)); }
+inline int setBit(int N, int pos) { return N = N | (1 << pos); }
+inline int toggleBit(int N, int pos) { return N = (N ^ (1 << pos)); }
+
+LL gcd(LL a, LL b) { return b==0 ? a : gcd(b, a % b); }
+LL lcm(LL a, LL b) { return (a / gcd(a, b)) * b; }
+
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+///------------------------------------------------------------------------------------------------------------------------
+
+const int N = 1e6 + 10;
+const int MOD = 1000000007;
+
+void solve(int casenum)
+{
+    int days;
+    cin>>days;
+
+    int person = 0, rem_days = days;
+    double p = 0, prev_p = 1;
+    while(1)
+    {
+        double cur_p = double(rem_days) / double(days);
+        cur_p *= prev_p;
+        p = 1 - cur_p;
+        if(p >= 0.5) break;
+        person++;
+        prev_p = cur_p;
+        rem_days--;
+    }
+    cout<<"Case "<<casenum<<": "<<person<<endl;
+}
+
+int main()
+{
+    IOS;
+
+    #ifdef sinbadCP
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t = 1;
+    cin>>t;
+    for(int tt=1; tt<=t; tt++){
+        solve(tt);
+    }
+
+    return 0;
+}
+
+/*
+
+Link : https://lightoj.com/problem/birthday-paradox
+
+Sol:
+
+Problem wants us to find us the minimum number of people need to invite to a party such that 
+the probability of at least two people in the party having the same birthday is at least 0.5.
+We will solve this problem by complementary theorem. We will find the number of unique birthdays
+and then subtract it from 1. So, the remaining birthdays must contains duplicate means they
+must have at least two people sharing same birthday. So, now lets assume that the year has
+365 days and the number of people are 30. As each perosn will have a birthday which is not same
+with any other person means each day will be assign to only 1 person.
+For the 1st person we have 365 days available and the probability is 365/365.
+For the 2nd person we have 364 days available and the probability is 364/365.
+For the 3rd person we have 363 days available and the probability is 363/365.
+.
+.
+.
+For the 30th person we have 335 days available and the probability is 335/365.
+
+As we have to find the number of people so we will continue this procedure until we get
+the probability >= 0.5.
+
+*/
