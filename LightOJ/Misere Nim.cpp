7911ecc632d89201b5d3c0fe29ad7a1e5f2ee44b
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2022-10-05 14:41:19 +0600

    Game theory

---
+++ LightOJ/Misere Nim.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl            '\n'
+#define PI              (2.0*acos(0.0))
+#define PB              push_back
+#define MP              make_pair
+#define all(x)          x.begin(),x.end()
+#define asort(x)        sort(all(x));
+#define dsort(x)        sort(all(x), greater<int>())
+#define unq(x)          x.erase(unique(x.begin(),x.end()),x.end())
+#define popcountl       __builtin_popcountll
+#define popcount        __builtin_popcount
+#define now             cerr<<"Here"<<endl;
+#define mem(ara,val)    memset(ara,val,sizeof(ara))
+#define IOS             ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+inline bool checkBit(int N, int pos) { return (bool)(N & (1 << pos)); }
+inline int setBit(int N, int pos) { return N = N | (1 << pos); }
+inline int toggleBit(int N, int pos) { return N = (N ^ (1 << pos)); }
+
+LL gcd(LL a, LL b) { return b==0 ? a : gcd(b, a % b); }
+LL lcm(LL a, LL b) { return (a / gcd(a, b)) * b; }
+
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+///------------------------------------------------------------------------------------------------------------------------
+
+const int N = 1e6 + 10;
+const int MOD = 1000000007;
+
+void solve(int casenum)
+{
+
+    int n;
+    cin>>n;
+    int ans = 0, one = 0;
+    for(int i=1, x; i<=n; i++)
+    {
+        cin>>x;
+        ans ^= x;
+        if(x == 1) one++;
+    }
+
+    cout<<"Case "<<casenum<<": ";
+
+    if(one == n)
+    {
+        if(one % 2) cout<<"Bob"<<endl;
+        else cout<<"Alice"<<endl;
+    }
+    else
+    {
+        if(ans == 0) cout<<"Bob"<<endl;
+        else cout<<"Alice"<<endl;
+    }
+}
+
+int main()
+{
+    IOS;
+
+    #ifdef sinbadCP
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t = 1;
+    cin>>t;
+    for(int tt=1; tt<=t; tt++){
+        solve(tt);
+    }
+
+    return 0;
+}
+
+/*
+
+Link : https://lightoj.com/problem/misere-nim
+
+Sol : 
+
+In a misère game, the goal of the game is opposite, so the player who removes
+the last stick loses the game. It turns out that the misère nim game can be
+optimally played almost like the standard nim game.
+The idea is to first play the misère game like the standard game, but change
+the strategy at the end of the game. The new strategy will be introduced in a
+situation where each heap would contain at most one stick after the next move.
+In the standard game, we should choose a move after which there is an even
+number of heaps with one stick. However, in the misère game, we choose a move
+so that there is an odd number of heaps with one stick.
+This strategy works because a state where the strategy changes always
+appears in the game, and this state is a winning state, because it contains exactly
+one heap that has more than one stick so the nim sum is not 0.
+
+
+*/
+
