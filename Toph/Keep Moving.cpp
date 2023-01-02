3fc1f655c2c1cde8cd6754863a59e35cc98e5bf6
Atanu Saha <40003419+Atanusaha143@users.noreply.github.com>
2023-01-03 04:24:49 +0600

    DP, Game

---
+++ Toph/Keep Moving.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NL              '\n'
+#define PINF            INT_MAX
+#define NINF            INT_MIN
+#define sz(x)           x.size()
+#define PB              push_back
+#define MP              make_pair
+#define asort(x)        sort(all(x));
+#define PI              (2.0*acos(0.0))
+#define all(x)          x.begin(),x.end()
+#define Now             cerr<<"Here"<<endl;
+#define For(i, j, k)    for(int i = j; i <= k; i++)
+#define Rof(i, j, k)    for(int i = j; i >= k; i--)
+#define mem(ara,val)    memset(ara,val,sizeof(ara))
+#define dsort(x)        sort(all(x), greater<int>())
+#define unq(x)          x.erase(unique(x.begin(),x.end()),x.end())
+#define IOS             ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair <int,int> PII;
+typedef pair <LL,LL> PLL;
+
+#define popcountl       __builtin_popcountll
+#define popcount        __builtin_popcount
+inline int setBit(int N, int pos) { return N = N | (1 << pos); }
+inline int toggleBit(int N, int pos) { return N = (N ^ (1 << pos)); }
+inline bool checkBit(int N, int pos) { return (bool)(N & (1 << pos)); }
+
+LL gcd(LL a, LL b) { return b==0 ? a : gcd(b, a % b); }
+LL lcm(LL a, LL b) { return (a / gcd(a, b)) * b; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+LL  my_rand(LL l, LL r) { return uniform_int_distribution<LL>(l, r) (rng); }
+
+#define deb(x) cerr << #x <<" "; _print(x); cerr << endl;
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+///---------------------------------------------------------------------------------------------------------------------------------
+///---------------------------------------------------------------------------------------------------------------------------------
+
+const int N = 1e6 + 10;
+const int MOD = 1000000007;
+
+int n,m,x;
+vector<int>adj[N];
+int dp[N][2];
+
+int fun(int node, int player)
+{
+    if(sz(adj[node])==0)
+    {
+        if(player==0) return 0;
+        return 1;
+    }
+    if(dp[node][player]!=-1) return dp[node][player];
+    if(player==0)
+    {
+        int val=0;
+        for(int v:adj[node])
+        {
+            val=max(val,fun(v,player^1));
+        }
+        return dp[node][player]=val;
+    }
+    else
+    {
+        int val=1;
+        for(int v:adj[node])
+        {
+            val=min(val,fun(v,player^1));
+        }
+        return dp[node][player]=val;
+    }
+}
+
+void clean()
+{
+    For(i,1,n)
+    {
+        if(sz(adj[i])) adj[i].clear();
+        For(j,0,1)
+        {
+            dp[i][j]=-1;
+        }
+    }
+}
+
+void solve(int casenum)
+{   
+    cin>>n>>m>>x;
+    clean();
+    int u,v;
+    For(i,1,m)
+    {
+        cin>>u>>v;
+        adj[u].PB(v);
+    }
+    int ans=fun(x,0);
+    cout<<"Case "<<casenum<<": ";
+    if(ans) cout<<"Yes"<<NL;
+    else cout<<"No"<<NL;
+}
+
+int main()
+{
+    // IOS;
+
+    #ifdef sinbadCP
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int t = 1;
+    cin>>t;
+    for(int tt=1; tt<=t; tt++){
+        solve(tt);
+    }
+
+    return 0;
+}
+
+/*
+
+Link: https://toph.co/p/keep-moving
+
+*/
